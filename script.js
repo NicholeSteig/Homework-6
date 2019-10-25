cb9b9f143046caa2f54eb865d40cc695fa4091bd
Nyx <nichole.steig@gmail.com>
2019-10-25 13:40:09 -0700

    search works!

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-function buildURL() {
-var queryURL = 
-};
+//var weatherKey = "ee07ff4c3d03e19a43009d1b835b10fc"
 
-$("button").on("click", function() {
-    var person = $(this).attr("data-person");
+$("button").on("click", function () {
+    event.preventDefault();
+
+    var city = $("#city-to-search").val();
+
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + ",us,&appid=ee07ff4c3d03e19a43009d1b835b10fc";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+    });
 });
